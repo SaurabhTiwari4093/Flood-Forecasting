a9b200ed267ba2467b07874a278bfc96347482f9
Saurabh <saurabhtiwari4093@gmail.com>
2023-11-24 02:28:16 +0530

    Updated final

--- data_manipulation_scripts/population_gen.py
+++ data_manipulation_scripts/population_gen.py
@@ -5,11 +5,11 @@ import csv
 population = []
 
 def get_city_opendata(city, country):
-    tmp = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&q=%s&sort=population&facet=country&refine.country=%s'
-    cmd = tmp % (city, country)
+    tmp = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-500/records?refine=country:%s&refine=ascii_name:%s'
+    cmd = tmp % (country, city)
     res = requests.get(cmd)
     dct = json.loads(res.content)
-    out = dct['records'][0]['fields']
+    out = dct['results'][0]['population']
     return out
 
 csv_file = "finalfinal.csv"
@@ -20,9 +20,10 @@ with open(csv_file, 'r') as f:
         if row[0] != "city":
             # print(row[0])
             try:
-                pop = get_city_opendata(row[0], 'in')
-                population.append(pop["population"])
+                pop = get_city_opendata(row[0], 'India')
+                population.append(pop)
             except:
+                print("error in", row[0])
                 population.append(0)
 
 # population = [1,2,3,4,5,6,7,8]
@@ -31,3 +32,5 @@ with open('pop.csv', 'w') as f:
     writer = csv.writer(f)
     for val in population:
         writer.writerow([val])
+
+# Corrected some spelling and add some populations in the csv file manually
